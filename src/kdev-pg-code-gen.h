0bb2800344fc848875c91b813b8b5a25e6b1468a
Jakob Petsovits <jpetso@gmx.at>
2006-05-24 16:45:30 +0000

    Following Roberto's suggestion from the kdevelop-devel mailing list: Update the copyright headers so that it contains my name in the files that I messed with.

--- src/kdev-pg-code-gen.h
+++ src/kdev-pg-code-gen.h
@@ -1,5 +1,6 @@
 /* This file is part of kdev-pg
    Copyright (C) 2005 Roberto Raggi <roberto@kdevelop.org>
+   Copyright (C) 2006 Jakob Petsovits <jpetso@gmx.at>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public

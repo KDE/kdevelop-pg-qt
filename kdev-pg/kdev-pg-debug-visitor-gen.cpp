debedd42e00cddbfb7c32fb688a4178e17867ef3
Niko Sams <niko.sams@gmail.com>
2008-06-07 07:00:51 +0000

    debug visitor improvement: If you add the contents-string to the constructor it will print the actual strings - not just the positions

--- kdev-pg/kdev-pg-debug-visitor-gen.cpp
+++ kdev-pg/kdev-pg-debug-visitor-gen.cpp
@@ -29,7 +29,8 @@ void GenerateDebugVisitor::operator()()
   out << "class " << globalSystem.exportMacro << " DebugVisitor: public DefaultVisitor {" << endl
       << "public:" << endl;
 
-  out << "DebugVisitor(KDevPG::TokenStream *str) : m_str(str), m_indent(0) {}" << endl;
+  out << "DebugVisitor(KDevPG::TokenStream *str, const QString& content = QString())" << endl;
+  out << "    : m_str(str), m_indent(0), m_content(content) {}" << endl;
   GenerateDebugVisitorRule gen(out);
   for( World::SymbolSet::iterator it = globalSystem.symbols.begin();
        it != globalSystem.symbols.end(); it++ )
@@ -39,6 +40,7 @@ void GenerateDebugVisitor::operator()()
 
   out << "private:" << endl << "KDevPG::TokenStream *m_str;" << endl;
   out << "int m_indent;" << endl;
+  out << "QString m_content;" << endl;
   out << "};" << endl;
 }
 
@@ -58,9 +60,32 @@ void GenerateDebugVisitorRule::operator()(QPair<QString,
   out << "qint64 beginLine,endLine,beginCol,endCol;" << endl;
   out << "m_str->startPosition(node->startToken, &beginLine, &beginCol);" << endl;
   out << "m_str->endPosition(node->endToken, &endLine, &endCol);" << endl;
+  out << "QString tokenString;" << endl;
+  out << "if (!m_content.isEmpty()) {" << endl;
+  out << "    KDevPG::TokenStream::Token startToken = m_str->token(node->startToken);" << endl;
+  out << "    KDevPG::TokenStream::Token endToken = m_str->token(node->endToken);" << endl;
+  out << "    int begin = startToken.begin;" << endl;
+  out << "    int end = endToken.end;" << endl;
+  out << "    if (end-begin > 30) {" << endl;
+  out << "        tokenString = m_content.mid(begin, 10);" << endl;
+  out << "        tokenString += \" ...\";" << endl;
+  out << "        tokenString += QString(\"%1 more\").arg(end-begin-20);" << endl;
+  out << "        tokenString += \"... \";" << endl;
+  out << "        tokenString += m_content.mid(end-10, 10);" << endl;
+  out << "    } else {" << endl;
+  out << "        tokenString = m_content.mid(begin, end-begin+1);" << endl;
+  out << "    }" << endl;
+  out << "    tokenString = tokenString.replace('\\n', \"\\\\n\");" << endl;
+  out << "    tokenString = tokenString.replace('\\r', \"\\\\r\");" << endl;
+  out << "}" << endl;
   out << "qDebug() << QString().fill(' ', m_indent) << \"" << sym->mName << "[\" << m_str->token( node->startToken ).begin"
-      << " << \",\" << beginLine << \",\" << beginCol << \"] --- [\" << m_str->token( node->endToken ).end <<"
-      << " \",\" << endLine << \",\" << endCol << \"]\";" << endl;
+      << " << \",\" << beginLine << \",\" << beginCol << "
+      << "\"] --- [\" << m_str->token( node->endToken ).end <<"
+      << " \",\" << endLine << \",\" << endCol "
+      << "<< \"] \""
+      << " << tokenString"
+      << ";"
+      << endl;
   
   out << "m_indent++;";
   
